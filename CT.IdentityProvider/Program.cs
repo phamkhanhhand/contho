c53c8b10549eddd3b08746d1aa651c9d05c931ad
Pham Khanh Ha <phamkhanhhand@gmail.com>
2025-09-20 16:27:21 +0700

    identity provider

--- CT.IdentityProvider/Program.cs
+++ CT.IdentityProvider/Program.cs
@@ -1,8 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//controller phải đặt [Authorize] thì mới authen cái này
+// Cấu hình xác thực với JWT Token 
+//for logout require authen
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var pem = File.ReadAllText("Keys/public.pem");
+        var rsa = RSA.Create();
+        rsa.ImportFromPem(pem.ToCharArray());
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            //ValidateIssuerSigningKey = false
+            ValidateIssuer = false,     // Bỏ qua việc kiểm tra Issuer
+            ValidateAudience = false,   // Bỏ qua việc kiểm tra Audience
+            ValidateLifetime = true,    // Kiểm tra thời gian hết hạn của token
+            //ClockSkew = TimeSpan.Zero,   // Không có độ trễ cho thời gian hết hạn
+            ValidateIssuerSigningKey = true, // Bỏ qua kiểm tra chữ ký của token
+
+            IssuerSigningKey = new RsaSecurityKey(rsa),
+
+        };
+
+    });
+
 
 var app = builder.Build();
 
