f950ef5dbc576725fd58b28893b1cd347fbc339a
Pham Khanh Ha <phamkhanhhand@gmail.com>
2025-09-21 16:19:59 +0700

    add project budget

---
+++ CT.BudgetManagement/CT.BL/Business/adm_flex_valuesBL.cs
@@ -0,0 +1,47 @@
+﻿using CT.DL;
+using CT.Models.DTO;
+using CT.Models.Entity;
+using CT.Models.ServerObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT.BL
+{
+    public class adm_flex_valuesBL : BaseBL
+    { 
+        protected override Type EntityType
+        {
+            get
+            {
+                return typeof(adm_flex_values);
+            }
+        }
+        protected override List<Type> GetDetailBusinessType()
+        {
+            //return new List<Type>() { typeof(Contract) };
+            return new List<Type>() { };
+        }
+
+
+
+
+        public bool AddLink(ParentChildDTO linkSetDTO)
+        {
+            var rs = false;
+
+            adm_flex_valuesDL dl = new adm_flex_valuesDL();
+
+            //todo validate cant drop link if has value parent-child link.
+            //todo validate set_id is true
+
+            rs = dl.AddLink(linkSetDTO);
+
+            return rs;
+        }
+
+
+    }
+}
