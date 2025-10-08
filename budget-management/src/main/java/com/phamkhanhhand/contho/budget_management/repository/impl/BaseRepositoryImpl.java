03669166724b9aa967108b6cf958726db4e2ce8e
Pham Khanh Ha <phamkhanhhand@gmail.com>
2025-10-08 22:16:50 +0700

    remove transaction

--- budget-management/src/main/java/com/phamkhanhhand/contho/budget_management/repository/impl/BaseRepositoryImpl.java
+++ budget-management/src/main/java/com/phamkhanhhand/contho/budget_management/repository/impl/BaseRepositoryImpl.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
+//@Transactional
 public class BaseRepositoryImpl {
 
     @Autowired
